f7a06ca876ed44caadd386b63de541570ead82e7
Patrick Gibson <kd8ion@gmail.com>
2017-07-05 19:49:05 -0400

    changed timing on pir

--- pir.py
+++ pir.py
@@ -8,8 +8,8 @@ while True:
        if i==0:                 #When output from motion sensor is LOW
              print ("No intruders"),i
              #GPIO.output(3, 0)  #Turn OFF LED
-             time.sleep(0.1)
+             time.sleep(1)
        elif i==1:               #When output from motion sensor is HIGH
              print ("Intruder detected"),i
              #GPIO.output(3, 1)  #Turn ON LED
-             time.sleep(0.1)
+             time.sleep(1)
