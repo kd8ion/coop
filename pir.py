d076551c3f9d012f2af583c7f9fbdf477e2073be
Patrick Gibson <kd8ion@gmail.com>
2017-07-05 19:46:29 -0400

    initial

---
+++ pir.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.IN)         #Read output from PIR motion sensor
+while True:
+       i=GPIO.input(16)
+       if i==0:                 #When output from motion sensor is LOW
+             print ("No intruders"),i
+             #GPIO.output(3, 0)  #Turn OFF LED
+             time.sleep(0.1)
+       elif i==1:               #When output from motion sensor is HIGH
+             print ("Intruder detected"),i
+             #GPIO.output(3, 1)  #Turn ON LED
+             time.sleep(0.1)
